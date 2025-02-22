d18bf56d79ec3d5b84f01b3b47232c24ebf17341
Gamequic <demiancalleros1@gmail.com>
2025-02-21 21:12:22 -0700

    �� refactor: remove unused dependencies from previous project (, , , )

--- pkg/features/api.go
+++ pkg/features/api.go
@@ -3,14 +3,6 @@ package featuresApi
 import (
 	"storegestserver/pkg/features/auth"
 	authservice "storegestserver/pkg/features/auth/service"
-	"storegestserver/pkg/features/food"
-	foodservice "storegestserver/pkg/features/food/service"
-	"storegestserver/pkg/features/money"
-	moneyservice "storegestserver/pkg/features/money/service"
-	"storegestserver/pkg/features/orders"
-	ordersservice "storegestserver/pkg/features/orders/service"
-	"storegestserver/pkg/features/photos"
-	photoservice "storegestserver/pkg/features/photos/service"
 	"storegestserver/pkg/features/profiles"
 	profileservice "storegestserver/pkg/features/profiles/service"
 	"storegestserver/pkg/features/users"
@@ -23,10 +15,6 @@ import (
 func RegisterSubRoutes(router *mux.Router) {
 	userservice.InitUsersService()
 	authservice.InitAuthService()
-	moneyservice.InitMoneyService()
-	foodservice.InitFoodService()
-	ordersservice.InitOrdersService()
-	photoservice.InitPhotosService()
 	profileservice.InitProfileService()
 	session.InitSessionService()
 
@@ -34,9 +22,5 @@ func RegisterSubRoutes(router *mux.Router) {
 
 	users.RegisterSubRoutes(apiRouter)
 	auth.RegisterSubRoutes(apiRouter)
-	money.RegisterSubRoutes(apiRouter)
-	food.RegisterSubRoutes(apiRouter)
-	orders.RegisterSubRoutes(apiRouter)
-	photos.RegisterSubRoutes(apiRouter)
 	profiles.RegisterSubRoutes(apiRouter)
 }
